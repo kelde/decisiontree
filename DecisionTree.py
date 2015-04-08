a5ebc6d9737af035e7546bd5662376b09847fd86
Kenneth Elder <kelder86@gmail.com>
2015-04-08 07:49:39 -0400

    removed attempt at recursion

--- DecisionTree.py
+++ DecisionTree.py
@@ -5,98 +5,127 @@ from itertools import *
 import operator
 import numpy
 
-class DecisionTree:
+class InteractiveDecisionTree:
 	
-	def __init__(self, filePath):
-		self.nodes = []
-		self.fieldNames = ['Diagnosis', 'FVC', 'FEV1', 'PerformanceStatus', 'Pain', 'Haemoptyisis', 'Dyspnoea', 'Cough', 'Weakness', 'SizeOfTumor', 'Diabetes', 'MI', 'PAD', 'Smokes', 'Asthma', 'Age', 'Risk1Y']
-		self.preProcessData(filePath)
-		self.outcomeColumn = 'Risk1Y'
-		self.entropy = self.setEntropy(self.records)
-		self.excludedFields = [self.outcomeColumn]
-		self.currentAttribute = None
-		self.setRootNode()
-		self.processBranches(self.rootNode)
-		print str(self.nodes)
+	def __init__(self, filePath, outcomeField):
+		self.filePath = filePath
+		self.excludedFields = []
+		self.setConditions = []
+		self.setOutcomeField(outcomeField)
 
-	def preProcessData(self, filePath):
-		with open('thoracic_surgery.csv', 'rb') as csvfile:
+	def setFieldNames(self, fieldNames):
+		self.fieldNames = fieldNames
+
+	def loadFile(self):
+		with open(self.filePath, 'rb') as csvfile:
 			records = csv.DictReader(csvfile, delimiter=',', fieldnames=self.fieldNames)
 			self.records = [x for x in records]
-			for field in self.fieldNames:
-				if field == 'FVC' or field == 'FEV1' or field == 'Age':
-					self.binField(field)
-
+			self.currentSet = self.records
+	
 	def binField(self, column):
-		minim = math.floor(self.getMin(column))
-		maxim = math.ceil(self.getMax(column))
-		bins = self.createBins(int(minim), int(maxim))
+		vals = [x[column] for x in self.records]
+		minim = math.floor(float(min(vals)))
+		maxim = math.ceil(float(max(vals)))
+		diff = int(maxim) - int(minim)
+		stepsize = diff / 5
+		bins = range(minim, maxim + (1 * stepsize), stepsize)
 		vals = numpy.array([float(x[column]) for x in self.records])
 		inds = numpy.digitize(vals,bins)
 		for n in range(vals.size):
 			self.records[n][column] = str(bins[inds[n]-1]) + "-" + str(bins[inds[n]])
 
-	def getMin(self, column):
-		vals = [x[column] for x in self.records]
-		minim = float(min(vals))
-		return minim
+	def printSet(self):
+		print str(self.records)
 
-	def getMax(self, column):
-		vals = [x[column] for x in self.records]
-		maxim = float(max(vals))
-		return maxim
+	def setOutcomeField(self, outcomeField):
+		self.outcomeField = outcomeField
+		self.excludedFields.append(outcomeField)
 
-	def createBins(self, start, stop):
-		diff = int(stop) - int(start)
-		stepsize = diff / 5
-		bins = range(start, stop + (1 * stepsize), stepsize)
-		return bins
+	def setExcludedFields(self, excludedFields):
+		self.excludedFields = excludedFields
 
-	def setEntropy(self, records):
+	def setSetConditions(self, setConditions):
+		self.setConditions = setConditions
+
+	def setCurrentSet(self):
+		self.currentSet = self.records
+		for condition in self.setConditions:
+			self.currentSet = [x for x in self.currentSet if x[condition['name']] == condition['value']]
+
+	def calculateSetEntropy(self):
 		groups = []
 		pos = 0
 		neg = 0
-		
-		records = sorted(records, key = lambda k: k[self.outcomeColumn])
-		
-		for k, g in groupby(records, key = lambda k: k[self.outcomeColumn]):
+		records = sorted(self.currentSet, key = lambda k: k[self.outcomeField])
+		for k, g in groupby(records, key = lambda k: k[self.outcomeField]):
 			groups.append(list(g))
-		
+
 		for group in groups:
-			value = group[0][self.outcomeColumn]
+			value = group[0][self.outcomeField]
 			if value == 'T':
 				pos = float(len(group))
 			else:
 				neg = float(len(group))
-		
+
 		if pos == 0 or neg == 0:
 			print "May have encountered a leaf, percentage of positive and/or negative outcomes is zero"
 		
-		total = len(records)
+		total = len(self.currentSet)
+
+		self.pos = pos
+		self.neg = neg
+
 		pos = pos / total
 		neg = neg / total
+
 		entropy = ((-pos) * (math.log(pos) / math.log(2))) - (neg * (math.log(neg) / math.log(2))) 
+
+		self.entropy = entropy
+
+	def printExcludedFields(self):
+		print "Fields currently excluded as potential nodes: " + str(self.excludedFields)
+
+	def printSetConditions(self):
+		print "Conditions placed on current set: "
+		print str(self.setConditions)
+
+	def printEntropy(self):
+		print "Positive outcomes in given set: " + str(self.pos)
+		print "Negative outcomes in given set: " + str(self.neg)
+		print "Entropy of given set is: " + str(self.entropy)
+
+	def getHighestGain(self):
+
+		gains = []
+		fields = [x for x in self.fieldNames if x not in self.excludedFields]
 		
-		return entropy
+		for field in fields:
+			gain = self.calculateGain(field)
+			gains.append( { 'name' : field, 'gain' : gain } )
+		maxGain = sorted(gains, key = lambda k: k['gain'], reverse=True)[0]
+		
+		self.maxGain = maxGain
 
-	def calculateGain(self, records, field, entropy):
-		total = float(len(records))
+	def calculateGain(self, field):
+		total = float(len(self.currentSet))
 		valueVars = []
-		records = sorted(records, key = lambda k: k[field])
+		records = sorted(self.currentSet, key = lambda k: k[field])
+		#print "Total records in set: " + str(total)
+		print "Attribute is: " + str(field)
 		for valueKey, valueGroup in groupby(records, key = lambda k: k[field]):
-
 			pos = 0
 			neg = 0
-
-			valueGroup = sorted(list(valueGroup), key = lambda k: k[self.outcomeColumn])
-			
-			for outcomeKey, outcomeGroup in groupby(valueGroup, key = lambda k: k[self.outcomeColumn]):
+			valueGroup = sorted(list(valueGroup), key = lambda k: k[self.outcomeField])
+			#print "Total number with value " + valueKey + ": " + str(len(valueGroup))
+			for outcomeKey, outcomeGroup in groupby(valueGroup, key = lambda k: k[self.outcomeField]):
 				outcomeGroup = list(outcomeGroup)
-				value = outcomeGroup[0][self.outcomeColumn]
+				value = outcomeGroup[0][self.outcomeField]
 				if value == 'T':
 					pos = float(len(outcomeGroup)) / len(valueGroup)
+					#print "Total number with value " + valueKey + " and outcome T: " + str(len(outcomeGroup))
 				if value == 'F':
 					neg = float(len(outcomeGroup)) / len(valueGroup)
+					#print "Total number with value " + valueKey + " and outcome F: " + str(len(outcomeGroup))
 			if pos == 0:
 				posComponent = 0
 			else:
@@ -106,135 +135,38 @@ class DecisionTree:
 			else:
 				negComponent = neg * (math.log(neg) / math.log(2))
 			entropySv = posComponent - negComponent
-			valueAttributes = {'key' : valueKey, 'sv' : len(valueGroup), 's' : total, 'pos' : pos, 'neg' : neg, 'entropy' : entropySv}
+			#print "Entropy of the set with value: " + valueKey + ": " + str(entropySv)
+			valueAttributes = {'sv' : len(valueGroup), 's' : total, 'entropy' : entropySv}
 			valueVars.append(valueAttributes)
-		gainSa = entropy
+		gainSa = self.entropy
 		for val in valueVars:
-	 		valueComponent = -(val['sv'] / val['s']) * val['entropy']
-			gainSa = gainSa + valueComponent
+			gainSa = gainSa + (-(val['sv'] / val['s']) * val['entropy'])
+		print "Gain for this attribute: " + str(gainSa)
 		return gainSa
 
-	def getHighestGain(self, records):
-		#print "Placeholder"
-		gains = []
-		fields = [x for x in self.fieldNames if x not in self.excludedFields]
-		for field in fields:
-			gain = self.calculateGain(records, field, self.entropy)
-			gains.append({ 'name' : field, 'gain' : gain})
-		maxGain = sorted(gains, key = lambda k: k['gain'], reverse=True)[0]
-		return maxGain
+	def printMaxGain(self):
+		print "Attribute with maximum gain is: " + str(self.maxGain)
 
-	def setRootNode(self):
-		#print "Placeholder"
-		maxGain = self.getHighestGain(self.records)
-		node = maxGain['name']
-		self.rootNode = node
-		self.nodes.append({ 'name' : node, 'branch' : 'Root' })
-		self.setRootNodeBranches()
+	def setNodeBranches(self, attributeName):
+		self.branches = list(set([x[attributeName] for x in self.records]))
 
+	def printBranches(self):
+		print str(self.branches)
 
-
-	def setDefaultCategory(self):
-		print "Placeholder"
-
-	def setLeaf(self):
-		print "Placeholder"
-
-	def setNode(self, nodename, branchName, childNode):
-		for i in range(len(self.nodes)):
-			if self.nodes[i]['name'] == nodename:
-				print "Adding childnode here: " + childNode
-				try:
-					self.nodes[i]['childnodes'].append({'name': childNode, 'branch': branchName})
-					self.setNodeBranches(nodename, childNode)
-				except KeyError:
-					self.nodes[i]['childnodes'] = []
-					self.nodes[i]['childnodes'].append({'name':childNode, 'branch':branchName})
-					self.setNodeBranches(nodename, childNode)
-
-
-
-
-		
-	def processBranches(self, nodename):
-		print "Placeholder"
-
-		for node in self.nodes:
-			if node['name'] == nodename:
-				branches = node['branches']
-				for branch in branches:
-					# calculate the sv set
-					sv = self.calculateSv(branch['name'], nodename)
-					numExamples = len(sv);
-					print "Branch " + branch['name'] + " has: " + str(len(sv)) + "examples"
-					# if sv is empty
-					if len(sv) == 0:
-						# set the default category here
-						self.setDefaultCategory(branch['name'], nodename)
-						continue
-					#if sv only contains examples from one classification
-					elif len(list(set([x[self.outcomeColumn] for x in sv]))) == 1:
-						classification = [x[self.outcomeColumn] for x in sv][0]
-						self.setBranchLeaf(branch['name'], nodename, classification)
-						continue
-					else:
-						# remove the attribute from the set 
-						self.excludedFields.append(nodename)
-						maxGain = self.getHighestGain(sv)
-						print str(maxGain)
-						childNode = maxGain['name']
-						branchName = branch['name']
-						print "Adding childNode: " + childNode + ", Nodename: " + nodename
-						self.setNode(nodename, branchName, childNode)
-						# self.calculateSv() with the new node
-
-
-	def calculateSv(self, branchname, nodename):
-		print str(nodename)
-		sv = [x for x in self.records if x[nodename] == branchname]
-		return sv
-		# get the set of examples from self.records that have value v for attribute a
-		
-	def setDefaultCategory(self, nodename, branchname):
-		for i in range(len(self.nodes)):
-			if self.nodes[i]['name'] == nodename:
-				branches = self.nodes[i]['branches']
-				for j in range(len(branches)):
-					if self.nodes[i]['branches'][j]['name'] == branchname:
-						self.nodes[i]['branches'][j]['leaf'] = 'Default Category'
-
-			
-
-
-
-
-
-	def setRootNodeBranches(self):
-		root = self.nodes[0]
-		root['branches'] = []
-		branches = list(set([x[self.rootNode] for x in self.records]))
-		for branch in branches:
-			root['branches'].append({'name': branch})
-
-	def setNodeBranches(self, nodename, childNode):
-		for i in range(len(self.nodes)):
-			if self.nodes[i]['name'] == nodename:
-				childNodes = self.nodes[i]['childnodes']
-				for j in range(len(childNodes)):
-					if self.nodes[i]['childnodes'][j]['name'] == childNode:
-						self.nodes[i]['childnodes'][j]['branches'] = []
-						branches = list(set([x[childNode] for x in self.records]))
-						self.nodes[i]['childnodes'][j]['branches'].append(branches)
-
-
-
-
-
-	def setBranchLeaf(self, branchname, nodename, classification):
-		for i in range(len(self.nodes)):
-			if self.nodes[i]['name'] == nodename:
-				branches = self.nodes[i]['branches']
-				for j in range(len(branches)):
-					if self.nodes[i]['branches'][j]['name'] == branchname:
-						self.nodes[i]['branches'][j]['leaf'] = classification
-
+	def processBranch(self, branch):
+		print "Calculating nodes for branch: " + branch
+		self.printExcludedFields()
+		self.printSetConditions()
+		self.setCurrentSet()
+		if len(self.currentSet) == 0:
+			print "This branch leads to a default category leaf node"
+			self.pos, self.neg, self.maxGain, self.entropy = (None,)*4
+			return
+		elif len(list(set([x[self.outcomeField] for x in self.currentSet]))) == 1:
+			classification = [x[self.outcomeField] for x in self.currentSet][0]
+			print "This branch leads to a classification node: " + classification
+			self.pos, self.neg, self.maxGain, self.entropy = (None,)*4
+			return
+		else:
+			self.calculateSetEntropy()
+			self.getHighestGain()
